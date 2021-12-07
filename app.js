71708cfe5914ce4595d679ab68191d8a7a6d5c28
zarlishkotwal <you@example.com>
2021-12-08 00:30:14 +0500

    workcomplete

--- app.js
+++ app.js
@@ -58,15 +58,19 @@ button[0].onclick = ()=>{
     }
 
 }
-function myFunction(){
+ function myFunction(){
     var x = document.getElementById("myInput")
     if (x.type ==="password"){
         x.type = "text";
-        document.getElementById('hide').style.display = "inline-block"
-        document.getElementById('show').style.display = "none"
-    } else{
+        document.getElementById('hide').style.display = "inline-block  "
+        document.getElementById('show').style.display = "none  "
+    }
+    else{
         x.type = "passsword"
-        document.getElementById('hide').style.display = "none"
-        document.getElementById('show').style.display = "inline-block"
+        document.getElementById('hide').style.display = "none" 
+        document.getElementById('show').style.display = "inline-block "      
     }
 }
+
+
+    
